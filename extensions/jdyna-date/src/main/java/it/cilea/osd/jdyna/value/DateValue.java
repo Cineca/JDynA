a1c56b04678c85bbcfdcd94b7ad2ea3a44202368
Luigi Andrea Pascarelli <pascarelli@cilea.it>
2010-08-17 14:32:43 +0000

    git-svn-id: https://svn.cilea.it/repos/jdyna/trunk@12 ef69fe1a-9dc5-41fb-afb6-2994247eeae8

---
+++ extensions/jdyna-date/src/main/java/it/cilea/osd/jdyna/value/DateValue.java
@@ -0,0 +1,77 @@
+/*
+ * JDynA, Dynamic Metadata Management for Java Domain Object
+ *
+ * Copyright (c) 2008, CILEA and third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by CILEA.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License v3 or any later version, as published 
+ * by the Free Software Foundation, Inc. <http://fsf.org/>.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ *
+ */
+package it.cilea.osd.jdyna.value;
+
+import it.cilea.osd.jdyna.model.AValue;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@DiscriminatorValue(value="data")
+public class DateValue extends AValue<Date> {	
+	@Temporal(value=TemporalType.TIMESTAMP)
+	@Column(name="dateValue")
+	private Date real;
+	
+	private static final DateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
+
+	@Override
+	public Date getObject() {
+		return real;
+	}
+
+	@Override
+	public void setReal(Date real) {
+		this.real = real;
+		if (real != null)
+		{
+			sortValue = String.valueOf(real.getTime());
+		}
+	}
+	
+	@Override
+	public String[] getUntokenizedValue() {
+		return real!=null?new String[]{String.valueOf(real.getTime())}:null;
+	}
+	
+	@Override
+	public String[] getTokenizedValue() {
+		return real!=null?new String[]{dateFormatter.format(real)}:null;
+	}
+
+	@Override
+	public Date getDefaultValue() {
+		return new Date();
+	}
+}
---
+++ extensions/jdyna-date/src/main/java/it/cilea/osd/jdyna/widget/WidgetData.java
@@ -0,0 +1,166 @@
+/*
+ * JDynA, Dynamic Metadata Management for Java Domain Object
+ *
+ * Copyright (c) 2008, CILEA and third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by CILEA.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License v3 or any later version, as published 
+ * by the Free Software Foundation, Inc. <http://fsf.org/>.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ *
+ */
+package it.cilea.osd.jdyna.widget;
+
+
+import it.cilea.osd.jdyna.model.AValue;
+import it.cilea.osd.jdyna.model.AWidget;
+import it.cilea.osd.jdyna.service.IPersistenceDynaService;
+import it.cilea.osd.jdyna.util.ValidationMessage;
+import it.cilea.osd.jdyna.value.DateValue;
+
+import java.beans.PropertyEditor;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+
+
+/**
+ * Classe puntatore
+ * 
+ * @author biondo,pascarelli
+ * 
+ */
+@Entity
+@Table(name="dyna_widget_data")
+@NamedQueries( {  
+	@NamedQuery(name = "WidgetData.findAll", query = "from WidgetData order by id")
+    })
+public class WidgetData extends AWidget {
+		
+	private Integer minYear;
+	private Integer maxYear;
+	
+	/** se devono inserire l'ora o meno*/
+	private boolean time;
+		
+	
+	public long toMilliSecondi(int anno, int mese, int giorno) {
+		
+		GregorianCalendar data = new GregorianCalendar(anno,mese,giorno);
+		Date d1 = data.getTime();
+	    long l1 = d1.getTime();
+	    
+	    return l1;
+		
+	}
+	
+	public String getConfiguration() {
+		return "minYear="+minYear+";maxYear="+maxYear+";time="+time;
+	}
+
+
+    @Transient
+	public String getTriview() {
+		return "calendar";
+	}
+
+		
+	public Integer getMaxYear() {
+		return maxYear;
+	}
+
+
+
+	public void setMaxYear(Integer maxYear) {
+		this.maxYear = maxYear;
+	}
+
+
+
+	public Integer getMinYear() {
+		return minYear;
+	}
+
+
+
+	public void setMinYear(Integer minYear) {
+		this.minYear = minYear;
+	}
+
+
+    public boolean isTime()
+    {
+        return time;
+    }
+
+
+    public void setTime(boolean time)
+    {
+        this.time = time;
+    }
+
+	@Override
+	public PropertyEditor getPropertyEditor(IPersistenceDynaService applicationService) {
+		CustomDateEditor propertyEditor = new CustomDateEditor(
+				new SimpleDateFormat(time?"dd-MM-yyyy HH:mm":"dd-MM-yyyy"),true); 
+		return propertyEditor;
+	}
+
+	@Override
+	public AValue getInstanceValore() {
+		return new DateValue();
+	}
+	
+	@Override
+	public Class<? extends AValue> getValoreClass() {
+		return DateValue.class;
+	}
+	
+	@Override
+	public ValidationMessage valida(Object valore) {
+		if (valore == null) {
+			return null;
+		} else {
+			Date date = (Date) valore;
+			Date max = null;
+			Date min = null;
+			if (minYear != null) {
+				min = new GregorianCalendar(minYear,1,1).getTime();
+			}
+			if (maxYear != null) {
+				max = new GregorianCalendar(maxYear+1,1,1).getTime();
+			}
+			
+			if (minYear != null && date.compareTo(min) == -1) {
+				return new ValidationMessage("date.mustbeafter",new Object[]{min});
+			}
+			if (maxYear != null && date.compareTo(max) == 1) {
+				return new ValidationMessage("date.mustbebefor",new Object[]{max});
+			}
+			return null;
+		}
+		
+	}
+}
