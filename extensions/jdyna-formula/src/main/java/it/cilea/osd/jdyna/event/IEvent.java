b46da813ad31294536d60efd08f7e55d41e06728
Luigi Andrea Pascarelli <pascarelli@cilea.it>
2010-08-31 21:11:27 +0000

    git-svn-id: https://svn.cilea.it/repos/jdyna/trunk@60 ef69fe1a-9dc5-41fb-afb6-2994247eeae8

---
+++ extensions/jdyna-formula/src/main/java/it/cilea/osd/jdyna/event/IEvent.java
@@ -0,0 +1,9 @@
+package it.cilea.osd.jdyna.event;
+
+public interface IEvent {
+	/**
+	 * 
+	 * @return una descrizione testuale dell'evento occorso
+	 */
+	public String getDetail();
+}
---
+++ extensions/jdyna-formula/src/main/java/it/cilea/osd/jdyna/event/ISubscriber.java
@@ -0,0 +1,20 @@
+package it.cilea.osd.jdyna.event;
+
+
+/**
+ *	Interfaccia per i sottoscrittori, ogni oggetto che vuole essere un subscriber deve
+ *  implementare questa interfaccia.
+ *  
+ *   @author pascarelli
+ */
+public  interface ISubscriber<E extends IEvent> {
+	/** 
+	 * 	Metodo invocato dal gestore di eventi su tutti i subscriber
+	 *  
+	 *  @param arg0 la classe dell'evento che è stato scatenato
+	 *  @param arg1 l'instanza contenente i dettagli dell'evento occorso
+	 *  
+	 *  @author pascarelli
+	 *  */
+	public void receive(Class<E> classeEvento, E evento);
+}
