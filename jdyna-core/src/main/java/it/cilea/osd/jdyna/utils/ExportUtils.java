18b6a4c879b8b0fd24c03b3ddedd458fac9fed67
Luigi Andrea Pascarelli <pascarelli@cilea.it>
2010-10-14 08:29:09 +0000

    organize import
    git-svn-id: https://svn.cilea.it/repos/jdyna/trunk@148 ef69fe1a-9dc5-41fb-afb6-2994247eeae8

--- jdyna-core/src/main/java/it/cilea/osd/jdyna/utils/ExportUtils.java
+++ jdyna-core/src/main/java/it/cilea/osd/jdyna/utils/ExportUtils.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.dom4j.dom.DOMElement;
 import org.jdom.Element;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
